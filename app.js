482e691400e66b05377a320f2ec4485e7cdb1610
calmighted <manishh@tutanota.com>
2022-07-19 10:37:48 +0530

    dynamic port for heroku

--- app.js
+++ app.js
@@ -31,7 +31,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.set("json spaces", 4);
 
-app.listen(5000, () => {
+app.listen(process.env.PORT || 5000, () => {
 	console.log("Server running on port 5000");
 });
 
